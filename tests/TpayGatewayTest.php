703022c5c24e70ac5d63c1c50dcf76124a6a7650
Piotr Jóźwiak <piotr.jozwiak@tpay.com>
2018-06-29 11:12:04 +0200

    Test API access update

--- tests/TpayGatewayTest.php
+++ tests/TpayGatewayTest.php
@@ -18,9 +18,9 @@ class TpayGatewayTest extends GatewayTestCase
         $this->gateway = new TpayGateway($this->getHttpClient(), $this->getHttpRequest());
         $this->options = array(
             'rsaKey'           => 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0NCk1JR2ZNQTBHQ1NxR1NJYjNEUUVCQVFVQUE0R05BRENCaVFLQmdRQ2NLRTVZNU1Wemd5a1Z5ODNMS1NTTFlEMEVrU2xadTRVZm1STS8NCmM5L0NtMENuVDM2ekU0L2dMRzBSYzQwODRHNmIzU3l5NVpvZ1kwQXFOVU5vUEptUUZGVyswdXJacU8yNFRCQkxCcU10TTVYSllDaVQNCmVpNkx3RUIyNnpPOFZocW9SK0tiRS92K1l1YlFhNGQ0cWtHU0IzeHBhSUJncllrT2o0aFJDOXk0WXdJREFRQUINCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ',
-            'apiKey'           => 'ba9a05faa697f9b43f39b84933ff168e373c6496',
-            'apiPassword'      => 'XtCns9OAue8zSFJ',
-            'verificationCode' => 'b680517e034e6b820e92975b58fecaf0',
+            'apiKey'           => 'bda5eda723bf1ae71a82e90a249803d3f852248d',
+            'apiPassword'      => 'IhZVgraNcZoWPLgA',
+            'verificationCode' => '6680181602d396e640cb091ea5418171',
             'hashType'         => 'sha1',
         );
     }
